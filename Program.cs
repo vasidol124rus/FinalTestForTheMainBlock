c895d6110a96f104a28f83a0c8428f31812e1aec
Vasiliy Kuznetsov <vasekku25@gmail.com>
2024-03-26 16:48:05 +0700

    написал код, работает

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+string[] array = { "Hello", "2", "world", ":-)" };
+string[] newArray = new string[array.Length];
+int elementLength = 3;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= elementLength) newArray[i] = array[i];
+}
+Console.Write($"[" + String.Join(", ", array) + "] -> ");
+Console.WriteLine($"[" + String.Join(" ", newArray) + "]");
